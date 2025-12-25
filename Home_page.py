1f43227a50e589581a209c277511947a65cb87d1
RohitKrishnia <rohitkrishnia@gmail.com>
2025-12-26 00:03:29 +0530

    Remove unnecessary text from homepage

--- Home_page.py
+++ Home_page.py
@@ -3,7 +3,7 @@ import streamlit as st
 def main():
     st.set_page_config(page_title="Real Estate Transactions", layout="wide")
     st.write("# Welcome to Money management app 👋")
-    st.write("Checking something")
+ 
 
 
 if __name__ == "__main__":
